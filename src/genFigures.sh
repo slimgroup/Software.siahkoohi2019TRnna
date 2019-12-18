cbd6cefe50c5338d299a29780a38449ca00c2056
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2019-12-18 15:52:49 -0500

    Update genFigures.sh

--- src/genFigures.sh
+++ src/genFigures.sh
@@ -8,4 +8,4 @@ result_path=$HOME/$repo_name/outputs/$experiment_name/sample
 path_script=$HOME/$repo_name/src
 savepath=$HOME/$repo_name/outputs/$experiment_name/figs
 
-python show_prediction.py --hdf5path $result_path --savepath $savepath
+python path_script/show_prediction.py --hdf5path $result_path --savepath $savepath
