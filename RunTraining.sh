e5aca78aac5d33fc31e0dd819f4125c4662815d7
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2019-12-31 13:29:29 -0500

    Update RunTraining.sh

--- RunTraining.sh
+++ RunTraining.sh
@@ -8,6 +8,11 @@ path_data=$HOME/$repo_name/vel-model
 path_model=$HOME/$repo_name/outputs/$experiment_name
 mkdir -p $path_model
 
+if [ ! -f $path_data/vp_marmousi_bi ]; then
+	wget https://github.com/devitocodes/data/raw/master/Simple2D/vp_marmousi_bi \
+		-O $path_data/vp_marmousi_bi
+fi
+
 CUDA_VISIBLE_DEVICES=0 python $path_script/main.py --experiment_dir $experiment_name --phase train \
 --same_model_training 1 --correction_num 3 --training_fraction 2 --netEpoch 10 --epoch 500 --epoch_step 100 \
 --save_freq 1000  --print_freq 50 --checkpoint_dir $path_model/checkpoint --sample_dir $path_model/sample \
